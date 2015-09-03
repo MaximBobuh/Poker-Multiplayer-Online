ae0672a518cb54fe1068960a4d9e8ca7443176fd
Maxim <progon3@mail.ru>
2015-09-03 17:28:57 +0300

    Update combinations
    Add isFlash

--- Server/CardAction/combinations.cpp
+++ Server/CardAction/combinations.cpp
@@ -1,19 +1,43 @@
 #include <algorithm>
 #include <iostream>
 #include <vector>
+#include <set>
+#include <iterator>
 
 #include "combinations.h"
 
-
 int Combinations::defineCombination(const std::vector<int>& desk, std::string& name)
 {
-    Cards original;
-
-    //convert from int to Card
-    std::copy(desk.begin(), desk.end(), original.begin());
-
+    // Convert from int to Card.
+    Cards original(desk.begin(), desk.end());
     for(It pos = original.begin(); pos != original.end(); ++pos)
-        std::cout << pos->value << " " << pos->suit << std::endl;
+        std::cout << pos->suit << " " << pos->value << std::endl;
+    int p;
+
+    std:: cout << isFlash(original, p);
+
 
     return 1;
 }
+
+bool Combinations::isFlash(const Cards& original, int points)
+{
+    int val = -1;
+
+    It first, second;
+    first = second = original.begin();
+
+    std::advance(second,  4);
+    if(std::distance(second, original.end()) <= 0) throw "bad iterator";
+
+    while(second != original.end())
+    {
+        if(first->suit == second->suit)
+            val = first->value;
+
+        ++first;
+        ++second;
+    }
+    return val != -1;
+}
+
