6dd5814bddc82d7c465093dac01e58631eedad77
Maxim <progon3@mail.ru>
2015-09-11 18:37:41 +0300

    Update main.cpp

--- Server/CardAction/main.cpp
+++ Server/CardAction/main.cpp
@@ -3,17 +3,27 @@
 #include "combinations.h"
 #include "card_deck.h"
 
+
 int main()
 {
     srand((time(0)));
+
     std::vector<int> coll;
-    std::string result;
+    Card_Deck deck;
+    char ch;
 
-    Card_Desk deck;
-    for(int i = 0; i < 7; ++i)
-        coll.push_back(deck.getCard());
+    while(std::cin >> ch)
+    {
+        for(int i = 0; i < 7; ++i)
+            coll.push_back(deck.getCard());
 
-    Combinations::defineCombination(coll, result);
+        Cmb_Result result = Combinations::defineCombination(coll);
+        std::cout <<Cmb_Result::toString(result.name) << " " << result.value;
+
+        coll.clear();
+        deck.reset();
+    }
+
     //return 0;
 }
 
